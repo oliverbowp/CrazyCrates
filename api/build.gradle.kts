417306b8a0b4d9c53520278922e59144791d837b
Ryder Belserion <no-reply@ryderbelserion.com>
2023-05-25 15:51:05 -0400

    chore: clean up api script

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -2,16 +2,20 @@ plugins {
     id("paper-plugin")
 }
 
+repositories {
+    maven("https://api.modrinth.com/maven/")
+}
+
 dependencies {
     api(libs.configme)
 
-    compileOnly(libs.holographic.displays)
     compileOnly(libs.decent.holograms)
 
     compileOnly(libs.fancy.holograms)
+    compileOnly(libs.fancy.npcs)
 
     compileOnly(libs.cmi.api)
     compileOnly(libs.cmi.lib)
 
-    implementation("com.ryderbelserion.stick:stick-api:2.1.0.15")
+    implementation(libs.stick)
 }
