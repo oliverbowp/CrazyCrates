6bc3b39fffbf25cac2ed49600e5cd8cb60805d68
Ryder Belserion <no-reply@ryderbelserion.com>
2023-06-26 23:08:27 -0400

    import clean up

--- api/src/main/java/com/badbones69/crazycrates/api/storage/types/file/yaml/YamlUserManager.java
+++ api/src/main/java/com/badbones69/crazycrates/api/storage/types/file/yaml/YamlUserManager.java
@@ -5,7 +5,6 @@ import com.badbones69.crazycrates.api.storage.interfaces.UserManager;
 import com.badbones69.crazycrates.api.storage.objects.UserData;
 import com.badbones69.crazycrates.api.objects.Crate;
 import com.ryderbelserion.stick.core.storage.enums.StorageType;
-import com.ryderbelserion.stick.paper.Stick;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.ConfigurationSection;
