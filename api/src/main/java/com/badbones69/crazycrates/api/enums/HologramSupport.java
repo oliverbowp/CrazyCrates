5201afaef083c651ee4f0bad373e769f582035e8
Ryder Belserion <no-reply@ryderbelserion.com>
2023-06-26 23:04:17 -0400

    Disable internal/fancy holograms

--- api/src/main/java/com/badbones69/crazycrates/api/enums/HologramSupport.java
+++ api/src/main/java/com/badbones69/crazycrates/api/enums/HologramSupport.java
@@ -3,14 +3,13 @@ package com.badbones69.crazycrates.api.enums;
 import com.badbones69.crazycrates.api.holograms.interfaces.HologramManager;
 import com.badbones69.crazycrates.api.holograms.types.CMIHologramSupport;
 import com.badbones69.crazycrates.api.holograms.types.DecentHologramSupport;
-import com.badbones69.crazycrates.api.holograms.types.InternalHologramSupport;
 
 public enum HologramSupport {
 
     decent_holograms(DecentHologramSupport.class),
-    cmi_holograms(CMIHologramSupport.class),
+    cmi_holograms(CMIHologramSupport.class);
     //fancy_holograms(FancyHologramSupport.class),
-    internal_holograms(InternalHologramSupport.class);
+    //internal_holograms(InternalHologramSupport.class);
 
     private final Class<? extends HologramManager> classObject;
 
