972830e76af9611766e7724f846d40414764cdd8
Ryder Belserion <no-reply@ryderbelserion.com>
2023-07-04 21:32:33 -0400

    Add extra arguments

--- api/src/main/java/com/badbones69/crazycrates/api/commands/sender/args/builder/FloatArgument.java
+++ api/src/main/java/com/badbones69/crazycrates/api/commands/sender/args/builder/FloatArgument.java
@@ -1,17 +1,22 @@
 package com.badbones69.crazycrates.api.commands.sender.args.builder;
 
 import com.badbones69.crazycrates.api.commands.sender.args.ArgumentType;
-
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-public class IntArgument extends ArgumentType {
+public class FloatArgument extends ArgumentType {
 
     @Override
     public List<String> getPossibleValues() {
         List<String> numbers = new ArrayList<>();
 
-        for (int value = 1; value <= 100; value += 1) numbers.add(String.valueOf(value));
+        DecimalFormat decimalFormat = new DecimalFormat("0.0f");
+
+        for (float value = 0.1f; value <= 100.0f; value += 0.1f) {
+            String formattedValue = decimalFormat.format(value);
+            numbers.add(formattedValue);
+        }
 
         return numbers;
     }
